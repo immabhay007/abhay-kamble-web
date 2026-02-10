70d1d377f30637f847c34849a290309ab0af89ee
immabhay007 <abhaydagadu.kamble@alongx.com>
2026-02-10 19:59:19 +0530

    Initial commit - personal portfolio

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,11 @@
 import resume from '../assets/pdf/resume.pdf'
 import abhayMain from '../assets/png/abhay-main.png'
+import abhayMain2 from '../assets/png/abhay-main-2.png'
 
 export const headerData = {
     name: 'Abhay Kamble',
     title: "Full Stack Developer",
     desciption:"Full Stack Developer with strong expertise in .NET technologies, JavaScript, and modern frameworks. Experienced in building scalable web applications, integrating APIs, and managing databases across multiple projects.",
-    image: abhayMain,
+    image: abhayMain2,
     resumePdf: resume
 }
